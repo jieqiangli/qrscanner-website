690f20963a91fbb7c5e45e907797c310609fddf5
Jie Qiang Li <jieqiang.li@gmail.com>
2020-05-16 18:09:41 +0100

    fixed typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Privacy policy</h1>
-    <p>I needed this because the Play Store said I had to have a privacy policy URL. I don't collect any information, and everything is processed on your devie only.</p>
+    <p>I needed this because the Play Store said I had to have a privacy policy URL. I don't collect any information, and everything is processed on your device only.</p>
     <p>The source code is <a href="https://github.com/jieqiangli/QRScanner">on GitHub</a></p>
 
     <p>Now go do something great.</p>
